77983eca72e6039ba8b09974e0e3896df774b574
Gerard Gunnewijk <gerard.gunnewijk@fotofabriek.nl>
2024-08-31 20:10:37 +0200

    Add Tokenizer and PdfName tests

---
+++ tests/Synercoding.FileFormats.Pdf.Tests/Parsing/TokeniserTests.cs
@@ -0,0 +1,75 @@
+using Synercoding.FileFormats.Pdf.IO;
+using Synercoding.FileFormats.Pdf.Parsing;
+using System.Text;
+
+namespace Synercoding.FileFormats.Pdf.Tests.Parsing;
+
+public class TokeniserTests
+{
+    [Theory]
+    [InlineData("[", TokenType.BeginArray)]
+    [InlineData("true", TokenType.Boolean)]
+    [InlineData("false", TokenType.Boolean)]
+    [InlineData("123", TokenType.Integer)]
+    [InlineData("/Test", TokenType.Name)]
+    [InlineData("(Hello world!)", TokenType.StringLiteral)]
+    [InlineData("<48656C6C6F20776F726C6421>", TokenType.StringHex)]
+    [InlineData("<<", TokenType.BeginDictionary)]
+    [InlineData("1.2", TokenType.Real)]
+    [InlineData(">>", TokenType.EndDictionary)]
+    [InlineData("]", TokenType.EndArray)]
+    [InlineData("% this is a comment", TokenType.Comment)]
+    public void Test_For_Correct_TokenType(string asciiData, TokenType expected)
+    {
+        var provider = new PdfByteArrayProvider(Encoding.ASCII.GetBytes(asciiData));
+        var tokeniser = new Tokenizer(provider);
+
+        if (!tokeniser.TryGetNextToken(out Token? token, skipComments: false))
+            Assert.Fail("Could not get the next token.");
+
+        Assert.Equal(expected, token.TokenType);
+    }
+
+    [Theory]
+    [InlineData("1.2", 1.2)]
+    [InlineData(".2", 0.2)]
+    [InlineData("+1.2", 1.2)]
+    [InlineData("+.2", 0.2)]
+    [InlineData("-1.2", -1.2)]
+    [InlineData("-.2", -0.2)]
+    [InlineData("123456789.987645321", 123456789.987645321)]
+    [InlineData("+123456789.987645321", 123456789.987645321)]
+    [InlineData("-123456789.987645321", -123456789.987645321)]
+    [InlineData(".23456789", 0.23456789)]
+    [InlineData("+1.23456789", 1.23456789)]
+    public void Test_For_Real_Numbers(string asciiData, double expected)
+    {
+        var provider = new PdfByteArrayProvider(Encoding.ASCII.GetBytes(asciiData));
+        var tokeniser = new Tokenizer(provider);
+
+        if (!tokeniser.TryGetNextToken(out Token? token))
+            Assert.Fail("Could not get the next token.");
+
+        if (token is TokenReal real)
+            Assert.Equal(expected, real.Number, tolerance: 0.00000000001);
+        else
+            Assert.Fail($"Did not retrieve a real token, instead got a {token.GetType().Name}.");
+    }
+
+    [Theory]
+    [InlineData("true", true)]
+    [InlineData("false", false)]
+    public void Test_For_Boolean_Tokens(string asciiData, bool expected)
+    {
+        var provider = new PdfByteArrayProvider(Encoding.ASCII.GetBytes(asciiData));
+        var tokeniser = new Tokenizer(provider);
+
+        if (!tokeniser.TryGetNextToken(out Token? token))
+            Assert.Fail("Could not get the next token.");
+
+        if (token is TokenBoolean boolean)
+            Assert.Equal(expected, boolean.Value);
+        else
+            Assert.Fail($"Did not retrieve a boolean token, instead got a {token.GetType().Name}.");
+    }
+}
